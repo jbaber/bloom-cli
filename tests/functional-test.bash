6706b5f52266cf6a98dafa0b8e7087d1e84a15d4
John Baber-Lucero <git@frundle.com>
2023-12-20 23:54:08 +0000

    Add actual functional tests

--- tests/functional-test.bash
+++ tests/functional-test.bash
@@ -4,8 +4,30 @@ set -e
 
 exe="$PWD"/target/debug/bloom-cli
 tmp="$PWD"/target/tmp
-beef="$PWD"/tests/deadbeef
+mkdir -p "$tmp"
+deadbeef="$PWD"/tests/deadbeef
+beefs="$PWD"/tests/beefs
 
 # Create fresh and insert
-"$exe" -x "$tmp"/filter-1 -i "$beef" 2>/dev/null
-"$exe" -x "$tmp"/filter-1 -q "$beef" 2>/dev/null
+rm -f "$tmp"/filter-1
+[[ $("$exe" -v -x "$tmp"/filter-1 -i "$deadbeef" 2>&1) = "Creating a new filter at "* ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-1 -q "$deadbeef") = "IN" ]] || exit 1
+
+# Create blank, then insert
+rm -f "$tmp"/filter-2
+[[ $("$exe" -v -x "$tmp"/filter-2 2>&1) = "Creating a new filter at "* ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-2 -q "$deadbeef") = "NOT IN" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-2 -i "$deadbeef") = "" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-2 -q "$deadbeef") = "IN" ]] || exit 1
+
+# Create blank, add one at a time
+rm -f "$tmp"/filter-3
+[[ $("$exe" -v -x "$tmp"/filter-3 2>&1) = "Creating a new filter at "* ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-3 -q "$beefs") = "NOT IN" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-3 -q "$deadbeef") = "NOT IN" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-3 -i "$beefs") = "" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-3 -q "$beefs") = "IN" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-3 -q "$deadbeef") = "NOT IN" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-3 -i "$deadbeef") = "" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-3 -q "$deadbeef") = "IN" ]] || exit 1
+[[ $("$exe" -x "$tmp"/filter-3 -q "$beefs") = "IN" ]] || exit 1
